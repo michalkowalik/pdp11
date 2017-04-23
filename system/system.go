b86630b4726ec52ff5722531aea6c0e045503aa1
Michał Kowalik <mp.kowalik@gmail.com>
2017-04-23 23:18:43 +0200

    initial commit

---
+++ system/system.go
@@ -0,0 +1,10 @@
+package system
+
+// system definition.
+// Memory - static array, 4MB in size:
+var memory [4 * 1024 * 1024]byte
+
+// InitializeSystem initializes the emulated PDP-11/44 hardware
+func InitializeSystem() {
+
+}
