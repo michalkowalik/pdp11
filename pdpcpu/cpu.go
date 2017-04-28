746f427f72c904273fa1b64194e0bea3c16b107e
Michal Kowalik <mkowalik@kleiberweb.de>
2017-04-28 09:20:22 +0200

    small update on cpu

--- pdpcpu/cpu.go
+++ pdpcpu/cpu.go
@@ -13,24 +13,24 @@ type CPU struct {
 // cpu should be able to fetch, decode and execute:
 
 // Fetch next instruction from memory
-func (c CPU) Fetch() {
+func (c *CPU) Fetch() {
 	fmt.Printf("CPU Fetch\n")
 }
 
 //Decode fetched instruction
-func (c CPU) Decode() {
+func (c *CPU) Decode() {
 	fmt.Printf("Decode..\n")
 }
 
 // Execute decoded instruction
-func (c CPU) Execute() {
+func (c *CPU) Execute() {
 	fmt.Printf("Execute.. \n")
 }
 
 // helper functions:
 
 // DumpRegisters displays register values
-func (c CPU) DumpRegisters() {
+func (c *CPU) DumpRegisters() {
 	for i, reg := range c.registers {
 		fmt.Printf("R%d: 0x%x\n", i, reg)
 	}
---
+++ pdpcpu/instructions.go
@@ -0,0 +1,8 @@
+package pdpcpu
+
+// Definition of all PDP-11 CPU instructions
+// All should follow the func (*CPU) (int16) signature
+
+// helper functions:
+
+// mode returns the addressing mode for the argument allowing to decode the argument value
