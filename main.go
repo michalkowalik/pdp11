0cdc36a340396ca7a27f531a134511434079b706
Michał Kowalik <mp.kowalik@gmail.com>
2017-05-07 12:30:34 +0200

    add command line gui - initial commit

--- main.go
+++ main.go
@@ -2,11 +2,46 @@ package main
 
 import (
 	"fmt"
-	"pdp/system"
+
+	"log"
+
+	"github.com/jroimartin/gocui"
 )
 
 func main() {
-	fmt.Printf("Starting PDP-11/70 emulator..\n")
-	pdp := system.InitializeSystem()
-	pdp.Noop()
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln("Couldn't create gui!")
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+
+	// fmt.Printf("Starting PDP-11/70 emulator..\n")
+	// pdp := system.InitializeSystem()
+	// pdp.Noop()
+}
+
+// gocui layout
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("PDP 11/70", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Hello world!")
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
