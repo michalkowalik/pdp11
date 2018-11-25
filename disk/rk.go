7f8b1d551ad16ec7f5f109a7162b375a2f1d413e
Michal Kowalik <mkowalik@kleiberweb.de>
2018-11-25 23:22:15 +0100

    rk11 drive implementation started

--- disk/rk.go
+++ disk/rk.go
@@ -1,5 +1,11 @@
 package disk
 
+import (
+	"io/ioutil"
+)
+
+const rk5ImageLength = 2077696
+
 // RK11 disk controller
 type RK11 struct {
 	// Registers -> check description in attached markdown
@@ -19,7 +25,7 @@ type RK11 struct {
 	running bool
 
 	// we also somehow need the unibus here...
-	// think about channel communication with unibus.
+	Instructions chan Instruction
 }
 
 // RK05 disk cartridge
@@ -28,7 +34,72 @@ type RK05 struct {
 	locked bool
 }
 
+// Instruction - to provide unibus exchange channel
+// Read == false -> write operation
+// btw, it slowly looks like teletype and rk could implement the same interface.
+type Instruction struct {
+	Address uint32
+	Data    uint16
+	Read    bool
+}
+
+// New  returns new RK11 object
+func New() *RK11 {
+	r := RK11{}
+	r.Instructions = make(chan Instruction)
+	return &r
+}
+
 // Attach reads disk image file and loads it to memory
 func (r *RK11) Attach(drive int, path string) error {
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	unit := &RK05{
+		rdisk: buf,
+	}
+	r.unit[drive] = unit
 	return nil
+}
+
+// Run - Start the RK11
+// initialize the go routine to read from incoming channel
+func (r *RK11) Run() {
+	go func() {
+		instruction := <-r.Instructions
+		if instruction.Read {
+			//data := r.read(instruction.Address)
+			r.read(instruction.Address)
+			// send back to Unibus!
+		} else {
+			r.write(instruction.Address, instruction.Data)
+		}
+	}()
+}
+
+func (r *RK11) rkReady() {
+	// nothing to see here yet
+}
+
+func (r *RK11) rkNotReady() {
+	// nothing to see here yet
+}
+
+func (r *RK11) read(address uint32) uint16 {
+	return 0
+}
+
+func (r *RK11) write(address uint32, value uint16) {
+	// end up in panic?
+}
+
+// reset sets the drive to it's default values.
+// check bits meaning in attached documentation
+func (r *RK11) reset() {
+	r.RKDS = (1 << 11) | (1 << 7) | (1 << 6)
+	r.RKER = 0
+	r.RKCS = 1 << 7
+	r.RKWC = 0
+	r.RKBA = 0
 }
--- disk/rk.md
+++ disk/rk.md
@@ -7,6 +7,21 @@
 ### Drive Status Register (RKDS)
 * unibus address: 0777400
 * bits:
+    |bits|designation|description|
+    |----|-----------|-----------|
+    |0 - 3 | Sector Counter (SC)|current sector address of the seclected drive|
+    | 4 | SC = SA | Sector counter = Sector Address. Indicates that disk heads are positioned over the disk addr. currently kept in Sector Address|
+    | 5 | Write Protect Status (WPS) | 1  if disk in write protect mode|
+    | 6 | Read/Write/Seek Ready (W/R/S RDY)|Drive mechanism is not in motion, drive ready to receive next order|
+    | 7 | Drive Ready (DRY) | Drive ready to work and not in DRU condition|
+    | 8 | Sector Counter OK (SOK) | SC on selected drive not in process of changing, and can be examined. if not set, second attempt should be made|
+    | 9 | Seek Incomplete (SIN) | Seek function can not be completed. Cleared by reset|
+    | 10| Drive Unsafe (DRU) | Drive unable to complete operation. Reset may be required |
+    | 11| RK05 disk online | always set, to identify inserted disk as rk05|
+    | 12| Drive Power Low (DPL)| power loss sensed. reset may be required|
+    | 13 - 15| Identification of drive (ID) | if interrupt occures, those bits keep the binary representation of logical drive number, that caused the interrupt|
+
+        
 
 ### Error Register (RKER)
 * unibus address: 0777402
@@ -14,7 +29,11 @@
 
 ### Control Status Register (RKCS)
 * unibus address: 0777404
-* bits
+* bits:
+    |bits|designation|description|
+    |----|-----------|-----------|
+    | 0  | GO | Causes the control to carry the function of ID kept in bits 1-3. Remains set until control begins to respond to GO|
+    | 7  | Control Ready (RDY - write only)| Control ready to perform a function. Set by init, cleared by GO|
 
 ### Word Count Register (RKWC)
 * unibus address: 0777406
